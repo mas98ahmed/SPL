35eb071bb19c1a9e708ad00d56a9bf2b8dad0008
Luee Akasha <luee@post.bgu.ac.il>
2021-01-11 15:28:19 +0200

    Application logic was defind!

---
+++ Assignment4/Application.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def database_creation():
+    pass
+
+def create_connection():
+    pass
+
+def repoting():
+    pass
+
+def send_shipment(location, amount):
+    pass
+
+def receive_shipment(name, amount, date):
+    vaccine_controller = 
+    vaccine_controller.insert(name)
+
