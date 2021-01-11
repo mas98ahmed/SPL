7af41ddf2ce37a9d5c61486ad427d11ac9c3bdfd
Luee Akasha <luee@post.bgu.ac.il>
2021-01-11 21:09:29 +0200

    counting functions were implemented!

--- Assignment4/PersistenceLayer/DAO/logistic_DAO.py
+++ Assignment4/PersistenceLayer/DAO/logistic_DAO.py
@@ -6,7 +6,7 @@ Created on Mon Jan 11 13:27:56 2021
 """
 from PersistenceLayer.Repository import repo
 class _logistic_DAO:
-    def __init__(self, conn):
+    def __init__(self):
         self._conn = repo._conn
     
     def insert(self, logistic):
@@ -16,5 +16,19 @@ class _logistic_DAO:
             """, [logistic.id, logistic.name, logistic.count_sent, logistic.count_received]) 
         except Exception as error:
             print(error)
+    
+    def update_count_sent(self, _id, new_count_sent):
+        try:
+            self._conn.execute(""" UPDATE logistics SET count_sent=? where id=? """,[new_count_sent, _id])
+            pass
+        except Exception as error:
+            print(error)
         
+    def update_count_received(self, _id, new_count_received):
+        try:
+            self._conn.execute(""" UPDATE logistics SET count_received=? where id=? """,[new_count_received, _id])
+            pass
+        except Exception as error:
+            print(error)
+    
 logistic_DAO = _logistic_DAO()
