20ea2c97f403a531c90ba706e3c47d791c518141
Luee Akasha <luee@post.bgu.ac.il>
2021-01-13 13:23:45 +0200

    parsing text files fixed!

--- Assignment4/Application.py
+++ Assignment4/Application.py
@@ -7,7 +7,10 @@ This is a temporary script file.
 import sys
 from PersistenceLayer.Repository import repo
 
-def database_building(database_records):
+def database_building(database_records_as_strings):
+    database_records = None
+    for i in range(database_records_as_strings.count):
+        database_records.insert(database_records_as_strings[i].split(','))
     repo.create_tables()
     repo.store(database_records)
     pass
@@ -15,17 +18,17 @@ def database_building(database_records):
 
 def manage_orders(orders):
     for i in range(orders.count):
-        record = orders[i]
+        record = orders[i].split(',')
         if record.count == 2:
             repo.send_shipment(record[0], record[1])
         elif record.count == 3:
             repo.receive_shipment(record[0], record[1], record[2])
 
 def main(config_file, orders_file, output_file):
-    with open(config_file) as cfile:
+    with open(config_file,"r",encode="utf-8") as cfile:
         lines = cfile.readlines()
         database_building(lines)
-    with open(orders_file) as ofile:
+    with open(orders_file,"r",encode="utf-8") as ofile:
         lines = ofile.readlines()
         manage_orders(lines)
     pass
